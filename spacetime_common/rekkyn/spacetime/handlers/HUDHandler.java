f11f524550506acbe858f90d92e9f23232194a68
Rekkyn <reedesler@gmail.com>
2013-05-07 20:11:10 -0700

    Started work on the spacetime charge HUD meter

--- spacetime_common/rekkyn/spacetime/handlers/HUDHandler.java
+++ spacetime_common/rekkyn/spacetime/handlers/HUDHandler.java
@@ -2,9 +2,13 @@ package rekkyn.spacetime.handlers;
 
 import java.util.EnumSet;
 
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import rekkyn.spacetime.item.ISpacetimeCharge;
@@ -32,7 +36,7 @@ public class HUDHandler implements ITickHandler {
             if (player != null) {
                 currentItemStack = player.inventory.getCurrentItem();
                 
-                if (Minecraft.isGuiEnabled() && minecraft.inGameHasFocus) {
+                if (Minecraft.isGuiEnabled() /*&& minecraft.inGameHasFocus*/) {
                     if (currentItemStack != null) {
                         renderSpacetimeCharge(minecraft, player, currentItemStack, (Float) tickData[0]);
                     }
@@ -70,12 +74,42 @@ public class HUDHandler implements ITickHandler {
         
         if (display) {
             // setup render
+            GL11.glPushMatrix();
             ScaledResolution res = new ScaledResolution(minecraft.gameSettings, minecraft.displayWidth,
                     minecraft.displayHeight);
+            
+            minecraft.entityRenderer.setupOverlayRendering();
+
+            RenderHelper.enableGUIStandardItemLighting();
             FontRenderer fontRender = minecraft.fontRenderer;
             int width = res.getScaledWidth();
             int height = res.getScaledHeight();
-            minecraft.entityRenderer.setupOverlayRendering();
+            
+            GL11.glPushMatrix();
+            RenderHelper.enableGUIStandardItemLighting();
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+            GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+            
+            GL11.glEnable(2929 /*GL_DEPTH_TEST*/);
+          //Makes your image get draw on the top layer instead of being covered by other things.
+
+
+            
+
+            
+          GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+          //This is redundant if all 1.0F but changes the shade.
+          GL11.glBindTexture(3553, minecraft.renderEngine.getTexture("/mods/Spacetime/textures/gui/spacetimeInfuser.png"));
+          //Bind the texture to be used.
+          minecraft.ingameGUI.drawTexturedModalRect(0, 0, 0, 166, 12, 40);
+          //Draw the part you want from the sprite map.
+          
+          minecraft.ingameGUI.drawTexturedModalRect(0, 0, 12, 166, 12, 40);
+
+          
+
+
             
             // draw
             String text = SpacetimeChargeHandler.getCurrentCharge(player) + "/"
@@ -83,6 +117,16 @@ public class HUDHandler implements ITickHandler {
             int x = 100;
             int y = 200;
             fontRender.drawStringWithShadow(text, x, y, 0x2a2f9f);
+            
+
+            
+            GL11.glDisable(GL11.GL_LIGHTING);
+            GL11.glDisable(2929 /*GL_DEPTH_TEST*/);
+            
+            GL11.glPopMatrix();
+            GL11.glPopMatrix();
+
+
         }
     }
     
